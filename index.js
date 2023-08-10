a572d0f15e641b12ce65266fff88f0f249107248
Saubhagya111 <115341978+Saubhagya111@users.noreply.github.com>
2023-08-10 18:54:31 +0545

    Update index.js

--- index.js
+++ index.js
@@ -48,12 +48,14 @@ const port = process.env.PORT || 5000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGODB_URI);
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
+  
     );
   } catch (error) {
     console.log(error);
   }
 };
 
-start();
+start().then(()=>{
+   app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`) 
+              })
